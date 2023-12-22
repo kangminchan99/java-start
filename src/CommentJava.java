ad567902c6989d5669ccec9bfc2252038adc881a
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-12-22 23:40:01 +0900

    주석

---
+++ src/CommentJava.java
@@ -0,0 +1,16 @@
+public class CommentJava {
+    // psvm - public static void main(String[] args)을 자동으로 만들어준다
+    public static void main(String[] args) {
+        // sout - System.out.println();
+        System.out.println("hello java1");
+        System.out.println("hello java2");
+
+        // - 한 줄 주석(single line comment)
+        // /* ~ */ - 여러 줄 주석처리(multi ling comment)
+        /*
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+        */
+
+    }
+}
