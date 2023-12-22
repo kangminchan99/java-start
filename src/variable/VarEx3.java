91c640afae32b1ad05994ab2d1b3d00744df5fc6
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-12-23 01:43:29 +0900

    변수 연습문제

---
+++ src/variable/VarEx3.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long l = 10000000000L;
+        boolean b = true;
+        System.out.println("long : " + l);
+        System.out.println("boolean : " + b);
+    }
+}
