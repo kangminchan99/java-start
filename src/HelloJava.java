e8a20ed0c203ebbddb62354c9bd9e0054cbb6cd8
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-12-22 23:35:18 +0900

    hello java

---
+++ src/HelloJava.java
@@ -0,0 +1,9 @@
+public class HelloJava {
+    // public static void main - 시작점인 main 메서드
+    public static void main(String[] args){
+        // 자바 언어는 대소문자를 구분한다.
+        //
+        System.out.println("hello java");
+
+    }
+}
---
+++ src/HelloJava2.java
@@ -0,0 +1,9 @@
+public class HelloJava2 {
+    public static void main(String[] args){
+        // 위에서부터 순서대로 실행
+        // println - 자동 줄바꿈
+        System.out.println("hello java1");
+        System.out.println("hello java2");
+        System.out.println("hello java3");
+    }
+}
