834a880e6df13bd2a6c2057ab72074601cb6b4f2
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-12-23 00:38:59 +0900

    변수 선언과 초기화

---
+++ src/variable/Var6.java
@@ -0,0 +1,9 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        // 변수를 초기화 하지 않으면 주석을 풀 시 컴파일 오류 발생
+//        int a;
+//        System.out.println(a);
+    }
+}
