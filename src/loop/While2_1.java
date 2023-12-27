f34b28c493ff38a497752ae94a1146cc9194aa6c
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-12-27 22:27:27 +0900

    while문2

---
+++ src/loop/While2_1.java
@@ -0,0 +1,22 @@
+package loop;
+
+public class While2_1 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 1;
+        sum += i;
+        System.out.println(sum);
+        i++;
+
+        sum += i;
+        System.out.println(sum);
+        i++;
+
+        sum += i;
+        System.out.println(sum);
+
+
+
+
+    }
+}
---
+++ src/loop/While2_2.java
@@ -0,0 +1,18 @@
+package loop;
+
+public class While2_2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        sum += 1;
+        System.out.println(sum);
+
+        sum += 2;
+        System.out.println(sum);
+
+        sum += 3;
+        System.out.println(sum);
+
+
+
+    }
+}
