d386d5d90fd0fd60d4aad7668b505ccd477986e6
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-12-27 22:09:40 +0900

    while문1

---
+++ src/loop/While1_2.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class While1_2 {
+    public static void main(String[] args) {
+        int count = 0;
+
+        while (count < 4) {
+            count += 1;
+            System.out.println(count);
+        }
+
+
+
+    }
+}
