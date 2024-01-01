764f121b96b0dc3c7b4d9d2dba12fd85222873f7
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2024-01-02 05:02:37 +0900

    Scanner 학습

---
+++ src/scanner/Scanner1.java
@@ -0,0 +1,21 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Scanner1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("문자열을 입력");
+        String str = scanner.nextLine(); // 입력을 String으로 가져온다.
+        System.out.println("입력한 문자열" + str);
+
+        System.out.print("정수 입력");
+        int intValue = scanner.nextInt(); // 입력을 int로 가져온다.
+        System.out.println("입력한 정수" + intValue);
+
+        System.out.print("실수 입력");
+        double doubleValue = scanner.nextDouble(); // 입력을 double로 가져온다.
+        System.out.println("입력한 실수" + doubleValue);
+    }
+}
